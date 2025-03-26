b69773672e37cc3de087d70800007fdf2d67a3cb
e4508u <alexis.courault6@etu.univ-lorraine.fr>
2025-03-26 08:24:25 +0100

    added a help page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Dapp } from "./components/Dapp";
 import { NFTMarketplace } from "./components/NFTMarketplace";
+import { Help } from "./components/Help";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dapp />} />
+        <Route path="*" element={<Dapp />} />
         <Route path="/nft-marketplace" element={<NFTMarketplace />} />
+        <Route path="/help" element={<Help />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/Help.js
@@ -0,0 +1,38 @@
+import React from "react";
+import urlData from "../contracts/url.json";
+
+export const Help = () => {
+  return (
+    <div className="container p-4">
+      <h1>How to Configure Your MetaMask</h1>
+      <p>
+        To interact with our decentralized application, you must connect your MetaMask wallet to our custom network.
+      </p>
+      <h2>Steps to Set Up MetaMask</h2>
+      <ol>
+        <li>
+          <strong>Install MetaMask:</strong> If you haven't already, install the MetaMask extension in your browser.
+          <br /> See: <a href="https://metamask.io">metamask.io</a>
+        </li>
+        <li>
+          <strong>Add a new network:</strong>
+          <ol>
+            <li>Open MetaMask and click on the network dropdown at the top.</li>
+            <li>Select <em>Add Network</em> or <em>Custom RPC</em>.</li>
+            <li>
+              In the <em>New Network</em> form, enter the following RPC URL:
+              <br />
+              <code>{urlData.url}</code>
+            </li>
+            <li>Fill in any additional required fields (Chain ID, Symbol, etc.) if provided by your network administrator.</li>
+            <li>Save the network.</li>
+          </ol>
+        </li>
+        <li>
+          <strong>Connect your wallet:</strong> After adding the network, click on "<em>Connect Wallet</em>" in our Dapp.
+        </li>
+      </ol>
+      <p>If you need further help, please consult the documentation or contact support.</p>
+    </div>
+  );
+};
