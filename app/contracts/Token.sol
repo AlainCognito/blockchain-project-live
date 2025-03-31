2c90b55fd876f527b4c00191863d233874c7edd1
e4508u <alexis.courault6@etu.univ-lorraine.fr>
2025-03-31 08:17:26 +0200

    customized token name

--- app/contracts/Token.sol
+++ app/contracts/Token.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor() ERC20("My Hardhat Token", "MHT") {
+    constructor() ERC20("Jeff Pesos", "JFP") {
         // Mint 1,000,000 tokens (adjust decimals as needed)
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
+
     function decimals() public view virtual override returns (uint8) {
         return 0;
     }
